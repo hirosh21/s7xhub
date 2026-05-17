f71c24836166671e9c9d6d50cf622f3a7012925e
hirosh21 <mateushenriquecossta09@gmail.com>
2026-05-17 01:02:40 -0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -13,6 +13,6 @@
  █      ██████████████████████████████████████      █
  █                                                  █
  ████████████████████████████████████████████████████
+]]--
 
-
-return({fZ=function(N,N,V,d)d=0B1100;N=#V[0X1A];return d,N;end,uZ=function(N,V,d,n,u)local c,M=0B1010__01_1;repeat if c>22 then c=0X16;if not(V>0x8A)then for v=0X3c,128,0B1000100 do if v==0X3c then if not(V>94)then u=n[0B110001]();else local p=0B1000111;repeat if p==0B1111010 then break;else if p==71 then p,u=N:NZ(u,d,V,n,p);continue;end;end;until false;end;continue;else if v==0x80 then end;end;end;else M,u=N:SZ(u,n,V);if M==-0x01__ then return-1,u;end;end;continue;else if c<0B1010011 then break;end;end;until false;return nil,u;end,fU=function(N,N,V,d)d=V[0X2__6]();N=121;return N,d;end,DU=function
+return({fZ=function(N,N,V,d)d=0B1100;N=#V[0X1A];return d,N;end,uZ=function(N,V,d,n,u)local c,M=0B1010__01_1;repeat if c>22 then c=0X16;if not(V>0x8A)then for v=0X3c,128,0B1000100 do if v==0X3c then if not(V>94)then u=n[0B110001]();else local p=0B1000111;repeat if p==0B1111010 then break;else if p==71 then p,u=N:NZ(u,d,V,n,p);continue;end;end;until false;end;continue;else if v==0x80 then end;end;end;else M,u=N:SZ(u,n,V);if M==-0x01__ then return-1,u;end;end;continue;else if c<0B1010011 then break;end;end;until false;return nil,u;end,fU=function(N,N,V,d)d=V[0X2__6]();N=121;return N,d;end,DU=function -- (Seu código continua normalmente aqui...)
